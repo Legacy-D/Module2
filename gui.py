594610384fd094afc489188bb55ba8aa47db89d2
Legacy-D <70569130+Legacy-D@users.noreply.github.com>
2021-03-18 11:06:37 +0530

    Add files via upload

---
+++ gui.py
@@ -0,0 +1,6 @@
+import tkinter as tk
+window=tk.Tk()
+window.title("Hello World!!!")
+mylabel=tk.Label(window,text="Hello World!!",font=("unbuntu",60,"bold"),fg="blue")
+mylabel.pack()
+window.mainloop()
